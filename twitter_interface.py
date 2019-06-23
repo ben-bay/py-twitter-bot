420b8090479baca9799d2d40a40182acefcdaf41
Benjamin Bay <benbayml@gmail.com>
2019-06-22 18:30:20 -0700

    improved tweepy error message

--- twitter_interface.py
+++ twitter_interface.py
@@ -6,12 +6,13 @@ import tweepy
 
 def get_api(tokens):
     try:
-        auth = tweepy.OAuthHandler(tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
-        auth.set_access_token(tokens.ACCESS_KEY, tokens.ACCESS_SECRET)
+        auth = tweepy.OAuthHandler(tokens.consumer_key, tokens.consumer_secret)
+        auth.set_access_token(tokens.access_key, tokens.access_secret)
         api = tweepy.API(auth)
         return api
     except tweepy.error.TweepError as e:
-        print(f"\nError: Have you replaced the ?s in tokens.py?\nTraceback:\n{e}")
+        if e.api_code == 89:
+            print(f"\nError: Have you replaced the ?s in tokens.py?\nTraceback:\n{e}")
         raise
     except:
         print(f"Unexpected error: {sys.exc_info()[0]}")
@@ -22,7 +23,8 @@ def tweet(api, text):
     try:
         return api.update_status(text)
     except tweepy.error.TweepError as e:
-        print(f"\nError: Have you replaced the ?s in tokens.py?\nTraceback:\n{e}")
+        if e.api_code == 89:
+            print(f"\nError: Have you replaced the ?s in tokens.py?\nTraceback:\n{e}")
         raise
     except:
         print(f"Unexpected error: {sys.exc_info()[0]}")
